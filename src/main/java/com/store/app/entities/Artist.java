64c1b1fa8d49b05e03e68e9cd97f1d5ce144c84e
Lagro222 <wbenkhaira@gmail.com>
2026-07-16 00:12:47 +0000

    add: Artist entity

---
+++ src/main/java/com/store/app/entities/Artist.java
@@ -0,0 +1,40 @@
+package  com.store.app.entities;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+// import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "artists")
+public class Artist{
+  
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id_artist ;
+
+ private String name ;
+ private String genre;
+ private String country;
+
+@ManyToMany(mappedBy = "albums")
+@JsonIgnore
+ List<Album> albums = new ArrayList<>(); 
+  
+}
