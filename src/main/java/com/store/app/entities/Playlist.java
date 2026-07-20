0c2965d926ac656db145514f329895b1ea982a95
Lagro222 <wbenkhaira@gmail.com>
2026-07-20 19:53:12 +0000

    add: playlists entity , relation with users

---
+++ src/main/java/com/store/app/entities/Playlist.java
@@ -0,0 +1,40 @@
+package com.store.app.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Playlist
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "playlists")
+public class Playlist {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id_playlist;
+
+  private String name ;
+  private PlaylistType type;
+
+  @ManyToOne
+  @JoinColumn(name = "id_user")
+  private User user;
+  
+}
+
+public enum PlaylistType{
+  GLOBAL,
+  USER
+}
