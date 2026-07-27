56bdc9dfdd823df91a21e9dde4e6057dd7a86fbd
Lagro222 <wbenkhaira@gmail.com>
2026-07-27 22:46:42 +0000

    add: dto for artist requests

---
+++ src/main/java/com/store/app/DTOs/ArtistDTO/ArtistRequestDTO.java
@@ -0,0 +1,10 @@
+package  com.store.app.DTOs.ArtistDTO;
+
+/**
+ * ArtistRequestDTO
+ */
+/**
+ * ArtistRequestDTO
+ */
+public record ArtistRequestDTO(String name , String genre , String country ) {
+}
