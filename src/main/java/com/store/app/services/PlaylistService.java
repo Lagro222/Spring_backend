afb0dd199c6d2ef7ea6df849eaabbac34079b904
Lagro222 <wbenkhaira@gmail.com>
2026-08-01 22:08:26 +0000

    add: crud function for playlist functions

--- src/main/java/com/store/app/services/PlaylistService.java
+++ src/main/java/com/store/app/services/PlaylistService.java
@@ -21,4 +21,16 @@ public class PlaylistService {
   public Playlist getById(Long id){return playlist_repo.findById(id).orElseThrow(() -> new RuntimeException("no such playlist"));}
   public List<Playlist> getByTitle(String title){return playlist_repo.findByName(title);}
 
+  public Playlist create_playlist(Playlist new_playlist){
+    return playlist_repo.save(new_playlist);
+  }
+
+  public Playlist update_playlist(Long id,Playlist updated){
+    Playlist target_playlist = playlist_repo.findById(id).orElseThrow(()-> new RuntimeException("no such playlist !! updating failed"))
+  }
+
+  public void delete_playlist(Long id){
+    Playlist exist = playlist_repo.findById(id).orElseThrow(()-> new RuntimeException("no such playlist ! deleting failed"));
+    playlist_repo.delete(exist);
+  }
 }
