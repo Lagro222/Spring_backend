ab7b0451681dfe1737915862f51b7a2ff0377864
Lagro222 <wbenkhaira@gmail.com>
2026-08-01 21:25:14 +0000

    change: getmapping using searching with requestparam

--- src/main/java/com/store/app/controllers/AlbumController.java
+++ src/main/java/com/store/app/controllers/AlbumController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.store.app.services.AlbumService;
@@ -27,17 +29,22 @@ public class AlbumController {
     return album_service.getAll();
   }
 
-  @GetMapping("/{id}")
-  public Album getById(@PathVariable Long id){return album_service.getById(id);}
+  // @GetMapping("/{id}")
+  // public Album getById(@PathVariable Long id){return album_service.getById(id);}
+  
+  @GetMapping("/search")
+  public List<Album> getByTitle(@RequestParam(required = false) Long id,@RequestParam(required = false) String title){
+    if (id != null) return List.of(album_service.getById(id)); 
+    if (title != null) return album_service.getByName(title);
 
-  @GetMapping("/{title}")
-  public List<Album> getByTitle(@PathVariable String title){return album_service.getByName(title);}
+    return album_service.getAll();
+  }
  
   @PostMapping
   public Album create_Album(@RequestBody Album new_album){return album_service.create(new_album);}
 
-  @PostMapping("/update")
-  public Album update_Album(@RequestBody Album new_album){
-    return   album_service.update(new_album);
+  @PutMapping("/{id}")
+  public Album update_Album(@PathVariable Long id ,@RequestBody Album new_album){
+    return   album_service.update(id,new_album);
   }
 }
