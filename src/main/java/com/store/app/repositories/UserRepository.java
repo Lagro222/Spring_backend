e5745f3b1eed3e8dc3d8d790e0cbca04bd730b4e
Lagro222 <wbenkhaira@gmail.com>
2026-08-02 21:57:21 +0000

    add: findByName for searching by user name

--- src/main/java/com/store/app/repositories/UserRepository.java
+++ src/main/java/com/store/app/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.store.app.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,6 @@ import com.store.app.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
+  List<User> findByName(String name);
   
 }
