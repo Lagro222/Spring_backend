19dbaeaed906dc90de353ed91cd3bc36fdb0b1a2
Lagro222 <wbenkhaira@gmail.com>
2026-08-02 15:40:02 +0000

    add : postmapping because i forgot to add it

--- src/main/java/com/store/app/controllers/TrackController.java
+++ src/main/java/com/store/app/controllers/TrackController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +37,9 @@ public class TrackController {
 
     return track_service.getAll();
   }
+
+  @PostMapping
+  public Track create(@RequestBody Track new_track){return track_service.create_track(new_track);}
 
   @PutMapping("/update/{id}")
   public Track update(@PathVariable Long id, @RequestBody Track updating){return track_service.update_track(id, updating);}
