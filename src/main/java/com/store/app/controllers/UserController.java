035f5feff5314cfac2e4526ea73d24941d29674c
Lagro222 <wbenkhaira@gmail.com>
2026-08-12 09:21:44 +0000

    fix: small typo

--- src/main/java/com/store/app/controllers/UserController.java
+++ src/main/java/com/store/app/controllers/UserController.java
@@ -1,5 +1,7 @@
 package  com.store.app.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.store.app.services.UserService;
+import com.store.app.entities.User;
 
 /**
  * UserController
@@ -23,7 +26,7 @@ public class UserController {
   public List<User> getAll(){return user_service.getAll();}
 
   @GetMapping("/search/{name}")
-  public List<User> getByName(@PathVariable String name){ return user_service.getByName(name;}
+  public List<User> getByName(@PathVariable String name){ return user_service.getByName(name);}
 
   @GetMapping("/{id}")
   public User getById(Long id){return user_service.getById(id);}
