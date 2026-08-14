1981b082bafb332d019b697ddab39ba0ec60ff2c
Lagro222 <wbenkhaira@gmail.com>
2026-08-14 19:18:47 +0000

    add: post mapping for creating and liking features

--- src/main/java/com/store/app/controllers/UserController.java
+++ src/main/java/com/store/app/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +32,14 @@ public class UserController {
   @GetMapping("/{id}")
   public User getById(Long id){return user_service.getById(id);}
 
+  @PostMapping
+    public User create_user(@RequestBody User new_user){
+    return user_service.create_user(new_user);
+  }
+
+  @PostMapping("/{userId}/like/{trackId}")
+  public User likedTrack(@PathVariable Long trackId,@PathVariable Long userId){
+    return user_service.likedTrack(trackId, userId);
+  }
 
 }
