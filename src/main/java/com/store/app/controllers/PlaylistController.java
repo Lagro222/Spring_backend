c7130fc0c9a9a745bc6d59c1c4c237e5b35eea9b
Lagro222 <wbenkhaira@gmail.com>
2026-08-17 19:46:02 +0000

    add: post mapping for creating playlist and adding a track

--- src/main/java/com/store/app/controllers/PlaylistController.java
+++ src/main/java/com/store/app/controllers/PlaylistController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.store.app.entities.Playlist;
+import com.store.app.enums.PlaylistType;
 import com.store.app.services.PlaylistService;
 
 /**
@@ -21,6 +25,8 @@ public class PlaylistController {
   @Autowired
   private PlaylistService playlist_service;
 
+
+  //getMapping :
   @GetMapping
   public List<Playlist> getAll(){
     return playlist_service.getAll();
@@ -36,5 +42,15 @@ public class PlaylistController {
     return playlist_service.findByName(name);
   }
 
-  
+  //postmapping
+  @PostMapping
+  public Playlist create_playlist(@RequestParam(required = false) Long userId,@RequestBody Playlist new_playlist){
+    return playlist_service.create_playlist(userId,new_playlist);
+  }
+
+  @PostMapping("/{playlistId}/track/{trackId}")
+  public Playlist addTrack(@PathVariable Long playlistId,@PathVariable Long trackId){
+    return playlist_service.add_Track(playlistId, trackId);
+  }
+
 }
