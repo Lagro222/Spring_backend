546d3512f4b6300ca360b988f326f7ed375dd9b9
Lagro222 <wbenkhaira@gmail.com>
2026-08-19 22:49:39 +0000

    add: new entity for relation between playlist and track

---
+++ src/main/java/com/store/app/entities/PlaylistTrack.java
@@ -0,0 +1,45 @@
+package com.store.app.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * PlaylistTrack
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "playlist_track")
+public class PlaylistTrack {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "playlist_id")
+  private Playlist playlist_id;
+
+  @ManyToOne
+  @JoinColumn(name = "track_id")
+  private Track track_id;
+
+  @ManyToOne
+  @JoinColumn(name = "added_by")
+  private User addedBy;
+
+  private LocalDateTime addedAt;
+  private Integer postion;
+  
+}
