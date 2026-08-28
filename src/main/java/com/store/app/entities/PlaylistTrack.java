ce608a87cb4768c48c11f9a69d1066497e593eea
Lagro222 <wbenkhaira@gmail.com>
2026-08-28 19:03:10 +0000

    add: validation for playlist track

--- src/main/java/com/store/app/entities/PlaylistTrack.java
+++ src/main/java/com/store/app/entities/PlaylistTrack.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -42,7 +43,10 @@ public class PlaylistTrack {
   @JoinColumn(name = "added_by")
   private User addedBy;
 
+  
   private LocalDateTime addedAt;
+  
+  @NotNull(message = "Please enter a valid position")
   private Integer postion;
   
 }
