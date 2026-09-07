cce4780cf10405806c651ff13c08642a10a9f022
Lagro222 <wbenkhaira@gmail.com>
2026-09-07 22:39:42 +0000

    integrating the ownership inside the controller

--- src/main/java/com/store/app/controllers/AlbumController.java
+++ src/main/java/com/store/app/controllers/AlbumController.java
@@ -3,10 +3,12 @@ package  com.store.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +20,7 @@ import jakarta.validation.Valid;
 
 import com.store.app.DTOs.AlbumRequestDTO;
 import com.store.app.entities.Album;
+import com.store.app.entities.User;
 /**
  * AlbumController
  */
@@ -44,14 +47,26 @@ public class AlbumController {
     return album_service.getAll();
   }
  
-  @PostMapping
-  public Album create_Album(@Valid @RequestBody AlbumRequestDTO new_album){
-    return album_service.create(new_album);
+  @PostMapping("/create")
+  public Album create_Album(@Valid @RequestBody AlbumRequestDTO new_album,@AuthenticationPrincipal User current_user){
+    return album_service.create(new_album,current_user);
   }
 
 
-  @PutMapping("/{id}")
-  public Album update_Album(@PathVariable Long id ,@Valid @RequestBody AlbumRequestDTO new_album){
-    return   album_service.update(id,new_album);
+  @PutMapping("/update/{id}")
+  public Album update_Album(
+      @PathVariable Long id ,
+      @Valid @RequestBody AlbumRequestDTO new_album,
+      @AuthenticationPrincipal User current_user
+  ){ 
+        return   album_service.update(id,new_album,current_user);
   }
+
+  @DeleteMapping("/delete/{id}")
+  public void delete_Album(@PathVariable Long id,@AuthenticationPrincipal User current_user){
+    album_service.delete(id,current_user);
+  }
+
+
+
 }
