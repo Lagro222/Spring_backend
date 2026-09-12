bc39d1ade5157d8922adaf223c9125ed70f7319c
Lagro222 <wbenkhaira@gmail.com>
2026-09-12 15:31:22 +0000

    update function with authentification

--- src/main/java/com/store/app/controllers/UserController.java
+++ src/main/java/com/store/app/controllers/UserController.java
@@ -3,6 +3,7 @@ package  com.store.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -76,8 +77,12 @@ public class UserController {
 
   //PUT
   @PutMapping("/update/{userId}")
-  public User update_user(@PathVariable Long userId, @Valid @RequestBody UserRequestDTO updated){
-    return user_service.update_user(userId, updated);
+  public User update_user(
+      @PathVariable Long userId, 
+      @Valid @RequestBody UserRequestDTO updated,
+      @AuthenticationPrincipal  User current_user
+  ){
+    return user_service.update_user(userId, updated,current_user);
   }
 
   //DELETE
